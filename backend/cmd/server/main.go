11549a5cef32790c3ea57ad37f910d10ddac52fb
devfiqi <salmanfiqi@gmail.com>
2026-04-12 01:04:14 -0500

    implemented cors

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -11,6 +11,21 @@ import (
 	"scalelab-backend/internal/handlers"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 
@@ -19,7 +34,7 @@ func main() {
 
 	log.Println("server running on :8080")
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", corsMiddleware(mux))
 	if err != nil {
 		log.Fatal(err)
 	}
